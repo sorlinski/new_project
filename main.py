2db38b7aafdec5d2f6be939222e247454bc108e5
sorlinski <sorlinsk@uwo.ca>
2026-01-12 14:01:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+
+Start_year = int(input("Enter your birth year (e.g., 1995): "))
+
+end_year = int(input("Enter the current year (e.g., 2026): "))
+
+age = Strat_year - end_year
+
+print(f"You are {age} years old.")
