f047fc0b9604a81fa2231a88475ba52852520ff8
sorlinski <sorlinsk@uwo.ca>
2026-01-12 14:11:23 -0500

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,11 @@
+
+first_number = int(input("first number: "))
+second_number = int(input("second number: "))
+
+
+Sum = first_number + second_number
+Sub = first_number - second_number
+Mul = first_number * second_number
+Div = first_number / second_number 
+
+print(f"Sum: {Sum}, Sub: {Sub}, Mul: {Mul}, Div: {Div}")
